9a1694ca55c06497cd256d66041e75733d0efa83
BIT1120161929 <38394153+BIT1120161929@users.noreply.github.com>
2019-07-12 20:49:22 +0800

    Exercise4Finished

--- deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/StartModle/DActivity.java
+++ deprecated-chapter1-master/homework/src/main/java/chapter/android/aweme/ss/com/homework/StartModle/DActivity.java
@@ -3,6 +3,7 @@ package chapter.android.aweme.ss.com.homework.StartModle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import chapter.android.aweme.ss.com.homework.R;
@@ -13,11 +14,14 @@ public class DActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_d);
+        Log.d("D", "onClick: "+getTaskId());
         findViewById(R.id.btn_d2a).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DActivity.this,AActivity.class);
                 startActivity(intent);
+
+
             }
         });
     }
